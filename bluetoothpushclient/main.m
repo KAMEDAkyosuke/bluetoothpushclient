0d7430357f87a04fa1beb26ea8b6da2a72c862d8
KAMEDAkyosuke <kameda-kyosuke@kayac.com>
2013-01-24 16:43:09 +0900

    Initial Commit

---
+++ bluetoothpushclient/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  bluetoothpushclient
+//
+//  Created by OrganLounge on 2013/01/24.
+//  Copyright (c) 2013年 OrganLounge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
